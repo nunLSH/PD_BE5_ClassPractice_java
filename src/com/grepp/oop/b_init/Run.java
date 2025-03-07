2e2cab53869348e93f0f374840c82b2ef6033e86
imseohyeon <forestlight749@gmail.com>
2025-03-07 14:37:58 +0900

    (0307) 초기화 순서

---
+++ src/com/grepp/oop/b_init/Run.java
@@ -0,0 +1,10 @@
+package com.grepp.oop.b_init;
+
+public class Run {
+
+    public static void main(String[] args) {
+        A_init init = new A_init("임서현BBB");
+//        A_init.loadClass();
+    }
+
+}
