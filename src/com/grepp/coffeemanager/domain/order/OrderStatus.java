a633a4183af0841a17ae39e51d7edce6af728838
imseohyeon <forestlight749@gmail.com>
2025-03-10 12:41:45 +0900

    (0310) 상수(enum)을 활용한 다형성

--- src/com/grepp/coffeemanager/domain/order/OrderStatus.java
+++ src/com/grepp/coffeemanager/domain/order/OrderStatus.java
@@ -2,12 +2,12 @@ package com.grepp.coffeemanager.domain.order;
 
 import com.grepp.coffeemanager.domain.purchase.Purchase;
 
-// fixme : enum으로 리팩토링
-public class OrderStatus {
+// NOTE ENUM : 연관된 상수들의 집합
+public enum OrderStatus {
 
-    public static final OrderStatus OK = new OrderStatus(0, "주문 생성 성공");
-    public static final OrderStatus FAIL_SOLD_OUT = new OrderStatus(1, "재고 부족으로 주문을 취소합니다.");
-    public static final OrderStatus FAIL_SEASON = new OrderStatus(2, "비시즌 입니다.");
+     OK(0, "주문 생성 성공"),
+     FAIL_SOLD_OUT(1, "재고 부족으로 주문을 취소합니다."),
+     FAIL_SEASON(2, "비시즌 입니다.");
 
     private int code;
     private String desc;
