11a81e7eed1fdab488aa5a4e508d331bab960d3d
imseohyeon <forestlight749@gmail.com>
2025-03-11 09:07:40 +0900

    (0310) interface

---
+++ src/com/grepp/oop/f_interface/Run.java
@@ -0,0 +1,21 @@
+package com.grepp.oop.f_interface;
+
+public class Run {
+
+    public static void main(String[] args) {
+        StarLink smartPhone = new SmartPhone(
+            "애플",
+            "아이폰",
+            1,
+            "긱벤치",
+            "SKT");
+
+        smartPhone.connect();
+        smartPhone.disconnect();
+
+        SmartPhone phone = (SmartPhone) smartPhone;
+        phone.send();
+        phone.receive();
+        phone.encrypt();
+    }
+}
