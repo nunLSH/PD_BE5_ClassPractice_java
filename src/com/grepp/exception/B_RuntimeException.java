ffba327259d84c335b9154e8a19f62b85947194f
imseohyeon <forestlight749@gmail.com>
2025-03-11 11:34:16 +0900

    (0311) 모든 예외처리

--- src/com/grepp/exception/B_RuntimeException.java
+++ src/com/grepp/exception/B_RuntimeException.java
@@ -35,25 +35,28 @@ public class B_RuntimeException {
         try {
             // ClassCastException
             Object obj = new Object();
-//            String str = (String) obj;
+            // String str = (String) obj;
 
             // ArrayIndexOutOfBoundsException의
             int[] arr = new int[2];
-//            arr[2] = 10;
+            // arr[2] = 10;
 
             // NullPointerException
             String nullStr = null;
             // null이 담겨있는 참조변수로 속성이나 메서드를 사용하려 할 경우 발생하는 예외
             nullStr.equals("a");
 
-        } catch (ClassCastException e){
-            System.out.println("ClassCastException의 catch block");
-            System.out.println(e.getMessage());
-        } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("ArrayIndexOutOfBoundsException의 catch block");
-            System.out.println(e.getMessage());
-        } catch (NullPointerException e){
-            System.out.println("NullPointerException의 catch block");
+//        } catch (ClassCastException e){
+//            System.out.println("ClassCastException의 catch block");
+//            System.out.println(e.getMessage());
+//        } catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("ArrayIndexOutOfBoundsException의 catch block");
+//            System.out.println(e.getMessage());
+//        } catch (NullPointerException e){
+//            System.out.println("NullPointerException의 catch block");
+//            System.out.println(e.getMessage());
+            // 모든 예외처리
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
