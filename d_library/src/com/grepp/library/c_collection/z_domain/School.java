b46913dfc30f553fa6cad83b18f51bcca8102b9c
imseohyeon <forestlight749@gmail.com>
2025-03-12 12:01:35 +0900

    (0312) list CRUD

---
+++ d_library/src/com/grepp/library/c_collection/z_domain/School.java
@@ -0,0 +1,23 @@
+package com.grepp.library.c_collection.z_domain;
+
+public class School{
+
+    private String name;
+    private String address;
+    private Integer level;
+
+    public School(String name, String address, Integer level) {
+        this.name = name;
+        this.address = address;
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+            "name='" + name + '\'' +
+            ", address='" + address + '\'' +
+            ", level=" + level +
+            '}';
+    }
+}
