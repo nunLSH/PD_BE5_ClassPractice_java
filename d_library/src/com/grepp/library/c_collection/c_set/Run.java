cb13191acfd053e5709f2b4e2ad606e27b08be25
imseohyeon <forestlight749@gmail.com>
2025-03-13 10:38:55 +0900

    (0313) Set의 집합(합, 차, 교, 부분)

--- d_library/src/com/grepp/library/c_collection/c_set/Run.java
+++ d_library/src/com/grepp/library/c_collection/c_set/Run.java
@@ -12,6 +12,48 @@ public class Run {
 
 //        pracAdd();
 //        pracRemove();
+
+        List<String> firstList = List.of(
+            "임서현",
+            "김예원",
+            "박현도",
+            "김준형",
+            "안준희"
+        );
+        List<String> secondList = List.of(
+            "지지",
+            "쿠마",
+            "멘델",
+            "김예원",
+            "키아라"
+        );
+        Set<String> firstSet = new HashSet<>(firstList);
+        Set<String> secondSet = new HashSet<>(secondList);
+        pracSetOperation(firstSet, secondSet);
+    }
+
+    private static void pracSetOperation(Set<String> firstSet, Set<String> secondSet) {
+
+        // NOTE CC01. 합집합
+        Set<String> setAll = new HashSet<>();
+        setAll.addAll(firstSet);
+        setAll.addAll(secondSet);
+        System.out.println("===합집합===");
+        System.out.println(setAll);
+
+        // NOTE CC02. 차집합
+        System.out.println("===차집합===");
+        setAll.removeAll(secondSet);
+        System.out.println(setAll);
+
+        // NOTE CC03. 교집합
+        System.out.println("===교집합===");
+        firstSet.retainAll(secondSet);
+        System.out.println(firstSet);
+
+        // NOTE CC04. 부분집합 여부
+        System.out.println("===부분집합 여부===");
+        System.out.println(secondSet.containsAll(firstSet));
     }
 
     private static void pracRemove() {
