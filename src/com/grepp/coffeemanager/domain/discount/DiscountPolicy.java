17be4bd647300f6d33299ddd4d4ac68b75b8437d
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-17 20:48:52 +0900

    Delete src/com/grepp/coffeemanager directory

--- src/com/grepp/coffeemanager/domain/discount/DiscountPolicy.java
+++
@@ -1,9 +0,0 @@
-package com.grepp.coffeemanager.domain.discount;
-
-import com.grepp.coffeemanager.domain.order.Order;
-
-public interface DiscountPolicy {
-
-    int getDiscountAmount(Order order);
-
-}
